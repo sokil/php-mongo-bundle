45a9f9762fff30c2277903d173763dd05504260b
Sokil <dmytro.sokil@gmail.com>
2018-05-30 08:12:54 +0300

    Update Configuration.php

--- src/Sokil/Mongo/MongoBundle/DependencyInjection/Configuration.php
+++ src/Sokil/Mongo/MongoBundle/DependencyInjection/Configuration.php
@@ -5,11 +5,6 @@ namespace Sokil\Mongo\MongoBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
-/**
- * This is the class that validates and merges configuration from your app/config files
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
- */
 class Configuration implements ConfigurationInterface
 {
     /**
@@ -21,9 +16,9 @@ class Configuration implements ConfigurationInterface
         $rootNode = $treeBuilder->root('mongo');
 
         $rootNode
-                ->children()
-                    ->scalarNode('dsn')->end()
-                ->end();
+            ->children()
+                ->scalarNode('dsn')->end()
+            ->end();
 
         return $treeBuilder;
     }
